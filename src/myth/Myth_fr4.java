686ef6bd6fea9b8f7e542561c572294f02e36cf3
Artemis <72870438+ArtemisKap01@users.noreply.github.com>
2022-01-20 17:35:28 +0200

    Update Myth_fr4.java

--- src/myth/Myth_fr4.java
+++ src/myth/Myth_fr4.java
@@ -30,10 +30,10 @@ public class Myth_fr4 extends JFrame {
 		//omada btn//
 		ButtonGroup bg;
 		//apanthseis- sunolo 4//
-		String ap1[] = {"α) Αριάδνη"};
-	    String ap2[] = {"β) Φαίδρα"};
-	    String ap3[] = {"γ) Αντιόπη"};
-	    String ap4[] = {"δ) Ιφιγένεια "}; /*Swsto*/
+		String ap1[] = {"\u03B1) \u0391\u03C1\u03B9\u03AC\u03B4\u03BD\u03B7"};
+	    String ap2[] = {"\u03B2) \u03A6\u03B1\u03AF\u03B4\u03C1\u03B1"};
+	    String ap3[] = {"\u03B3) \u0391\u03BD\u03C4\u03B9\u03CC\u03C0\u03B7"};
+	    String ap4[] = {"\u03B4) \u0399\u03C6\u03B9\u03B3\u03AD\u03BD\u03B5\u03B9\u03B1 "}; /*Swsto*/
 	
 	
 	private static int score=0;
@@ -129,7 +129,7 @@ public class Myth_fr4 extends JFrame {
 	     lbl_q1.setBounds(26, 37, 192, 61);
 	     panel.add(lbl_q1);
 	     
-	     lblNewLabel = new JLabel(" Ποιά λυπήθηκε τελευταία στιγμή η θεά Άρτεμις και έβαλε πριν τη θυσία στη θέση της;");
+	     lblNewLabel = new JLabel("\u03A0\u03BF\u03B9\u03AC \u03BB\u03C5\u03C0\u03AE\u03B8\u03B7\u03BA\u03B5 \u03C4\u03B5\u03BB\u03B5\u03C5\u03C4\u03B1\u03AF\u03B1 \u03C3\u03C4\u03B9\u03B3\u03BC\u03AE \u03B7 \u03B8\u03B5\u03AC \u0386\u03C1\u03C4\u03B5\u03BC\u03B9\u03C2 \u03BA\u03B1\u03B9 \u03AD\u03B2\u03B1\u03BB\u03B5 \u03C0\u03C1\u03B9\u03BD \u03C4\u03B7 \u03B8\u03C5\u03C3\u03AF\u03B1 \u03C3\u03C4\u03B7 \u03B8\u03AD\u03C3\u03B7 \u03C4\u03B7\u03C2;");
 	     lblNewLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 21));
 	     lblNewLabel.setForeground(new Color(102, 102, 204));
 	     lblNewLabel.setBounds(36, 109, 847, 70);
@@ -148,7 +148,7 @@ public class Myth_fr4 extends JFrame {
 					Myth_fr5 frame1 = new Myth_fr5();
 					frame1.setVisible(true);
 					} else {
-						JOptionPane.showMessageDialog(null, "Παρακαλώ επιλέξτε μια από τις προτεινόμενες απαντήσεις");
+						JOptionPane.showMessageDialog(null, "\u03A0\u03B1\u03C1\u03B1\u03BA\u03B1\u03BB\u03CE \u03B5\u03C0\u03B9\u03BB\u03AD\u03BE\u03C4\u03B5 \u03BC\u03B9\u03B1 \u03B1\u03C0\u03CC \u03C4\u03B9\u03C2 \u03C0\u03C1\u03BF\u03C4\u03B5\u03B9\u03BD\u03CC\u03BC\u03B5\u03BD\u03B5\u03C2 \u03B1\u03C0\u03B1\u03BD\u03C4\u03AE\u03C3\u03B5\u03B9\u03C2");
 					}
 	     	}
 	     });
@@ -174,7 +174,7 @@ public class Myth_fr4 extends JFrame {
 		Exit.addMouseListener(new MouseAdapter() {
 		     @Override
 		     public void mouseClicked(MouseEvent arg0) {
-		    	 if(JOptionPane.showConfirmDialog(null, "Είστε σίγουροι πως θέλετε να κλείσετε την εφαρμογή?", "Κλείσιμο", JOptionPane.YES_NO_OPTION) == 0) {
+		    	 if(JOptionPane.showConfirmDialog(null, "\u0395\u03AF\u03C3\u03C4\u03B5 \u03C3\u03AF\u03B3\u03BF\u03C5\u03C1\u03BF\u03B9 \u03C0\u03C9\u03C2 \u03B8\u03AD\u03BB\u03B5\u03C4\u03B5 \u03BD\u03B1 \u03BA\u03BB\u03B5\u03AF\u03C3\u03B5\u03C4\u03B5 \u03C4\u03B7\u03BD \u03B5\u03C6\u03B1\u03C1\u03BC\u03BF\u03B3\u03AE?", "\u039A\u03BB\u03B5\u03AF\u03C3\u03B9\u03BC\u03BF", JOptionPane.YES_NO_OPTION) == 0) {
 		    		Myth_fr4.this.dispose(); 
 		    	 }
 		}
