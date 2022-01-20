1c76ec37c1c54daceb2742d380d9a878bb6fe1a8
Artemis <72870438+ArtemisKap01@users.noreply.github.com>
2022-01-20 12:36:19 +0200

    Update Cultu_fr4.java

--- src/cultu/Cultu_fr4.java
+++ src/cultu/Cultu_fr4.java
@@ -32,10 +32,10 @@ public class Cultu_fr4 extends JFrame {
 		//omada btn//
 		ButtonGroup bg;
 		//apanthseis- sunolo 4//
-		String ap1[] = {"α) ....."};
-	    String ap2[] = {"β) ap2"};
-	    String ap3[] = {"γ) ap3"};
-	    String ap4[] = {"δ) ap4"};
+		String ap1[] = {"\u03B1)"};
+	    String ap2[] = {"\u03B2)"};
+	    String ap3[] = {"\u03B3)"};
+	    String ap4[] = {"\u03B4)"};
 	
 	
 	private static int score=0;
@@ -145,7 +145,7 @@ public class Cultu_fr4 extends JFrame {
 				Cultu_fr5 frame1 = new Cultu_fr5();
 				frame1.setVisible(true);
 				} else {
-					JOptionPane.showMessageDialog(null, "Παρακαλώ επιλέξτε μια από τις προτεινόμενες απαντήσεις");
+					JOptionPane.showMessageDialog(null, "\u03A0\u03B1\u03C1\u03B1\u03BA\u03B1\u03BB\u03CE \u03B5\u03C0\u03B9\u03BB\u03AD\u03BE\u03C4\u03B5 \u03BC\u03B9\u03B1 \u03B1\u03C0\u03CC \u03C4\u03B9\u03C2 \u03C0\u03C1\u03BF\u03C4\u03B5\u03B9\u03BD\u03CC\u03BC\u03B5\u03BD\u03B5\u03C2 \u03B1\u03C0\u03B1\u03BD\u03C4\u03AE\u03C3\u03B5\u03B9\u03C2");
 				}
 	     	}
 	     });
@@ -171,7 +171,7 @@ public class Cultu_fr4 extends JFrame {
 		Exit.addMouseListener(new MouseAdapter() {
 		     @Override
 		     public void mouseClicked(MouseEvent arg0) {
-		    	 if(JOptionPane.showConfirmDialog(null, "Είστε σίγουροι πως θέλετε να κλείσετε την εφαρμογή?", "Κλείσιμο", JOptionPane.YES_NO_OPTION) == 0) {
+		    	 if(JOptionPane.showConfirmDialog(null, "\u0395\u03AF\u03C3\u03C4\u03B5 \u03C3\u03AF\u03B3\u03BF\u03C5\u03C1\u03BF\u03B9 \u03C0\u03C9\u03C2 \u03B8\u03AD\u03BB\u03B5\u03C4\u03B5 \u03BD\u03B1 \u03BA\u03BB\u03B5\u03AF\u03C3\u03B5\u03C4\u03B5 \u03C4\u03B7\u03BD \u03B5\u03C6\u03B1\u03C1\u03BC\u03BF\u03B3\u03AE", "\u039A\u03BB\u03B5\u03AF\u03C3\u03B9\u03BC\u03BF", JOptionPane.YES_NO_OPTION) == 0) {
 		    		Cultu_fr4.this.dispose(); 
 		    	 }
 		}
