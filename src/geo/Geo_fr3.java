a0b6968bc8f2b5685c83376ae9d638c3ae3a6661
Artemis <72870438+ArtemisKap01@users.noreply.github.com>
2022-01-20 16:03:10 +0200

    Update Geo_fr3.java

--- src/geo/Geo_fr3.java
+++ src/geo/Geo_fr3.java
@@ -30,10 +30,10 @@ public class Geo_fr3 extends JFrame {
 		//omada btn//
 		ButtonGroup bg;
 		//apanthseis- sunolo 4//
-		String ap1[] = {"α) Ρόδος"};
-	    String ap2[] = {"β) Λέσβος"};
-	    String ap3[] = {"γ) Εύβοια "}; /*Swsto*/
-	    String ap4[] = {"δ) Κεφαλλονιά"};
+		String ap1[] = {"\u03B1) \u03A4\u03C1\u03B9\u03C7\u03BF\u03BD\u03AF\u03B4\u03B1"};
+	    String ap2[] = {"\u03B2) \u0392\u03B9\u03C3\u03C4\u03C9\u03BD\u03AF\u03B4\u03B1"};
+	    String ap3[] = {"\u03B3) \u03A0\u03B1\u03BC\u03B2\u03CE\u03C4\u03B9\u03B4\u03B1"}; /*Swsto*/
+	    String ap4[] = {"\u03B4) \u039A\u03B5\u03C1\u03BA\u03AF\u03BD\u03B7"};
 	
 	
 	private static int score=0;
@@ -129,13 +129,13 @@ public class Geo_fr3 extends JFrame {
 	     lbl_q1.setBounds(26, 37, 192, 61);
 	     panel.add(lbl_q1);
 	     
-	     lblNewLabel = new JLabel(" Το δεύτερο μεγαλύτερο νησί της Ελλάδας σε έκταση μετά την Κρήτη είναι:");
+	     lblNewLabel = new JLabel("  \u0397 \u03BC\u03B5\u03B3\u03B1\u03BB\u03CD\u03C4\u03B5\u03C1\u03B7 \u03C3\u03B5 \u03B5\u03C0\u03B9\u03C6\u03AC\u03BD\u03B5\u03B9\u03B1 \u03BB\u03AF\u03BC\u03BD\u03B7 \u03C4\u03B7\u03C2 \u0395\u03BB\u03BB\u03AC\u03B4\u03BF\u03C2 \u03B5\u03AF\u03BD\u03B1\u03B9");
 	     lblNewLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 23));
 	     lblNewLabel.setForeground(new Color(0, 0, 102));
 	     lblNewLabel.setBounds(38, 109, 763, 70);
 	     panel.add(lblNewLabel);
 	     
-	     JButton next_btn = new JButton("Επόμενο");
+	     JButton next_btn = new JButton("Ξ•Ο€ΟΞΌΞµΞ½ΞΏ");
 	     next_btn.addMouseListener(new MouseAdapter() {
 	     	@Override
 	     	public void mouseClicked(MouseEvent e) {
@@ -147,7 +147,7 @@ public class Geo_fr3 extends JFrame {
 				Geo_fr4 frame1 = new Geo_fr4();
 				frame1.setVisible(true);
 				} else {
-					JOptionPane.showMessageDialog(null, "Παρακαλώ επιλέξτε μια από τις προτεινόμενες απαντήσεις");
+					JOptionPane.showMessageDialog(null, "\u03A0\u03B1\u03C1\u03B1\u03BA\u03B1\u03BB\u03CE \u03B5\u03C0\u03B9\u03BB\u03AD\u03BE\u03C4\u03B5 \u03BC\u03B9\u03B1 \u03B1\u03C0\u03CC \u03C4\u03B9\u03C2 \u03C0\u03C1\u03BF\u03C4\u03B5\u03B9\u03BD\u03CC\u03BC\u03B5\u03BD\u03B5\u03C2 \u03B1\u03C0\u03B1\u03BD\u03C4\u03AE\u03C3\u03B5\u03B9\u03C2");
 				}
 	     	}
 	     });
@@ -173,7 +173,7 @@ public class Geo_fr3 extends JFrame {
 		Exit.addMouseListener(new MouseAdapter() {
 		     @Override
 		     public void mouseClicked(MouseEvent arg0) {
-		    	 if(JOptionPane.showConfirmDialog(null, "Είστε σίγουροι πως θέλετε να κλείσετε την εφαρμογή?", "Κλείσιμο", JOptionPane.YES_NO_OPTION) == 0) {
+		    	 if(JOptionPane.showConfirmDialog(null, "\u0395\u03AF\u03C3\u03C4\u03B5 \u03C3\u03AF\u03B3\u03BF\u03C5\u03C1\u03BF\u03B9 \u03C0\u03C9\u03C2 \u03B8\u03AD\u03BB\u03B5\u03C4\u03B5 \u03BD\u03B1 \u03BA\u03BB\u03B5\u03AF\u03C3\u03B5\u03C4\u03B5 \u03C4\u03B7\u03BD \u03B5\u03C6\u03B1\u03C1\u03BC\u03BF\u03B3\u03AE?", "\u039A\u03BB\u03B5\u03AF\u03C3\u03B9\u03BC\u03BF", JOptionPane.YES_NO_OPTION) == 0) {
 		    		Geo_fr3.this.dispose(); 
 		    	 }
 		}
